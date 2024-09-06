8cd12aed5374d00f1747e17bd9077cded59047fe
Jerome Lecomte <jerle76@gmail.com>
2024-09-06 11:59:58 -0400

    Add 3rd tutorial level about moving your body

--- src/components/puppet.js
+++ src/components/puppet.js
@@ -1,2 +1,9 @@
 AFRAME.registerComponent('puppet', {
+  events: {
+    ttlreached: function(e) {
+      const el = e.detail.el
+      el.parentNode.removeChild(el);
+      el.setAttribute('health', { hp: 0 })
+    }
+  }
 })
